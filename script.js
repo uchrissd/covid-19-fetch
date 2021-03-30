0be75eb93cf6f394b250f6359a17154121453cda
uchrissd <uchrissd@protonmail.com>
2021-03-30 14:57:56 -0700

    added innerhtml function for deaths, updated css styling and created new html elements to organize a list of the data i want to display

--- script.js
+++ script.js
@@ -9,20 +9,31 @@ const fetchData = async (url) => {
   return await response.text();
 };
 
+const deaths = "";
+
 const csvUrl =
   "https://raw.githubusercontent.com/datadesk/california-coronavirus-data/master/latimes-state-totals.csv";
 
 fetchData(csvUrl).then((text) => {
   const data = d3.csvParse(text);
   console.log(data);
+
   const date = data[0]["date"];
   console.log(date);
   const deaths = data[0]["deaths"];
   console.log(deaths);
+  document.getElementById("deaths").innerHTML = deaths;
   const confirmedCases = data[0]["confirmed_cases"];
   console.log(confirmedCases);
+  document.getElementById("confirmed-cases").innerHTML = confirmedCases;
   const newCases = data[0]["new_confirmed_cases"];
   console.log(newCases);
+  document.getElementById("new-cases").innerHTML =
+    "+" + newCases + " new cases";
   const newDeaths = data[0]["new_deaths"];
   console.log(newDeaths);
+  let newElement = document.createElement("div");
+  console.log(newElement);
 });
+
+console.log(deaths + "this is deaths again");
