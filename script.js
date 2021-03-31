feff0f734c9b2d99d5461c8221d1907de4b5f71c
uchrissd <uchrissd@protonmail.com>
2021-03-31 14:42:07 -0700

    re-formatted numbers to display punctuation, added covid-19 vaccine map iframe, added map styling to center on html page

--- script.js
+++ script.js
@@ -17,22 +17,29 @@ const csvUrl =
 fetchData(csvUrl).then((text) => {
   const data = d3.csvParse(text);
   console.log(data);
-
   let date = data[0]["date"];
-  console.log(date);
   let deaths = data[0]["deaths"];
-  console.log(deaths);
-  document.getElementById("deaths").innerHTML = deaths;
-  document.getElementById("confirmed-cases").innerHTML = deaths;
+  const deathsFormatted = new Intl.NumberFormat().format(deaths);
+  console.log(deathsFormatted);
+  document.getElementById("deaths").innerHTML = deathsFormatted;
   let confirmedCases = data[0]["confirmed_cases"];
+  const confirmedCasesFormatted = new Intl.NumberFormat().format(
+    confirmedCases
+  );
   console.log(confirmedCases);
-  document.getElementById("confirmed-cases").innerHTML = confirmedCases;
-  let newCases = "+" + data[0]["new_confirmed_cases"] + " new cases ";
+  document.getElementById(
+    "confirmed-cases"
+  ).innerHTML = confirmedCasesFormatted;
+  let newCases = data[0]["new_confirmed_cases"];
   console.log(newCases);
-  document.getElementById("new-cases").innerHTML = newCases;
-  let newDeaths = "+" + data[0]["new_deaths"] + " new deaths";
+  const newCasesFormatted = new Intl.NumberFormat().format(newCases);
+  document.getElementById("new-cases").innerHTML =
+    "+" + newCasesFormatted + " new cases ";
+  let newDeaths = data[0]["new_deaths"];
+  const newDeathsFormatted = new Intl.NumberFormat().format(newDeaths);
   console.log(newDeaths);
-  document.getElementById("new-deaths").innerHTML = newDeaths;
+  document.getElementById("new-deaths").innerHTML =
+    "+" + newDeathsFormatted + " new deaths";
 });
 
 const vaccineUrl =
@@ -42,8 +49,13 @@ fetchData(vaccineUrl).then((text) => {
   const data = d3.csvParse(text);
   console.log(data);
   let dosesAdministered = data[0]["doses_administered"];
+  const dosesAdministeredFormatted = new Intl.NumberFormat().format(
+    dosesAdministered
+  );
   console.log(dosesAdministered);
-  document.getElementById("doses-administered").innerHTML = dosesAdministered;
+  document.getElementById(
+    "doses-administered"
+  ).innerHTML = dosesAdministeredFormatted;
   let newDosesAdministered = [0]["new_doses_administered"];
   console.log(newDosesAdministered);
 });
